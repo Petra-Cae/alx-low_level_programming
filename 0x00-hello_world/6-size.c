5d0d3d5e09f8f8efdedd36dd30d0c88dde7687ef
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2022-10-13 09:35:10 -0700

    Print var sizes in c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Print the size of various types on its computer
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	float b;
+	char c;
+	double d;
+
+	printf("Size of int: %d bytes\n", sizeof(a));
+	printf("Size of float: %d bytes\n", sizeof(b));
+	printf("Size of char: %d bytes\n", sizeof(c));
+	printf("Size of double: %d bytes\n", sizeof(d));
+	return (0);
+}
