36aab747f2efb644778c7584721fa1f0503ddb22
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2022-10-14 10:17:04 -0700

    Is it a positive or negative number?

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Positive or negative number
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d\n is positive", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d\n is zero", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d\n is negative", n);
+	}
+	return (0);
+}
