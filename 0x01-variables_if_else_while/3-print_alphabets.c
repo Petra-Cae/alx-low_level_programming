05692ab0c74406c3bb569fa2dd5ca6fba53acaf9
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2022-10-14 19:39:17 -0700

    Print the alphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphaBET
+ *
+ * Return:  Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	char b;
+
+	a = 'a';
+	b = 'A';
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	while (b <= 'Z')
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
