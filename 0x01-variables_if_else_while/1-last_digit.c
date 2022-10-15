7b96ed6ca553fe2c27bebec22482fe0a41253a3f
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2022-10-14 23:04:56 -0700

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	l = n % 10;
-
-	if (n == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, l);
-	}
-	else if (n > 5)
+	
+	if (n > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
+	else if (n == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, l);
 	else
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
