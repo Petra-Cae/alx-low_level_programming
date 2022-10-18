99a630ebda19ec98482373336ccd8a2a945cd268
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2022-10-18 14:35:54 -0700

    Is it lower?

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_islower - Checks for lowercase letters showing 1 if lowercase else 0
+ *@c: The character argument
+ *
+ *Return: 1 for lowercase and o for others
+ */
+int _islower(int c);
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
