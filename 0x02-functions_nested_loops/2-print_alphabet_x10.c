b61e6da341770cbcb42f05b67aa30b02fab64075
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2022-10-18 14:00:07 -0700

    10x alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*main - Prints the alphabets 10x
+*
+*/
+void print_alphabet_x10(void)
+{
+	char ab;
+	int counter;
+
+	for (counter = 0; counter < 10; counter++)
+	{
+		for (ab = 'a'; ab <= 'z'; ab++)
+		{
+			_putchar(ab);
+		}
+	_putchar('\n');
+	}
+}
