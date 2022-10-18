eeebddeab4f0755ed846cfe39bb07f3c5232d321
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2022-10-18 15:27:15 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of intergers
+ * @n: int to be examined
+ *
+ *Return: Absolute value of int
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
