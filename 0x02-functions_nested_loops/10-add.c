1712e31fadfc568888fda5bc5af49a6b86826444
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2022-10-18 17:34:53 -0700

    Addition of two

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- *add - Addition of two ints
- *@num1: Number to be added
- *@num2: Number to be added
+ * add - Addition of two ints
+ * @num: Number to be added
+ * @nm: Number to be added
  *
- *Return: num1 plus num2
+ * Return: num plus nm
  */
-int add(num1, num2)
+int add(num, nm)
 {
 	int sum;
 
-	sum = num1 + num2;
+	sum = num + nm;
 	return (sum);
 }
