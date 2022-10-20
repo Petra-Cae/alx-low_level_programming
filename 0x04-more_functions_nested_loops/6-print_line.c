bf2883cefb76e7e3b6c5768dab2656003c816585
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2022-10-20 09:36:25 -0700

    A line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*print_line - Draws a straight line in the terminal
+*@n: number of times _ should be printed
+*/
+void print_line(int n)
+{
+	int b;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (b = 1; b < n; b++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
