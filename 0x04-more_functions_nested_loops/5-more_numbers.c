15092857ff32aeced2e41c502112afef121a5851
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2022-10-20 09:05:13 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*more_numbers - Prints 10x the numbers 0 to 14
+*/
+
+void more_numbers(void)
+{
+	int c;
+	int count;
+
+	for (c = 0; c < 15; c++)
+	{
+		for (count = 0; count < 10; count++)
+		{
+			if (c > 9)
+			{
+				_putchar(c / 10 + '0');
+			}
+			_putchar(c % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
