43cfe069587cbc22b573450f4a1b57ed257f319c
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2022-10-20 11:08:19 -0700

    Interger

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_number - prints an integer
+* @n: integer to be printed
+* Return: 0
+*/
+
+void print_number(int n)
+{
+	unsigned int a = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		a = -a;
+	}
+	if (a / 10)
+	{
+		print_number(a / 10);
+	}
+	_putchar(a % 10 + '0');
+}
