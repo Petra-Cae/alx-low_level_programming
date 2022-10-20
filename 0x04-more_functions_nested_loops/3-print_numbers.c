93fc60214951d76796281b987dd69d584427fe19
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2022-10-20 08:23:08 -0700

    putchar

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,12 @@
 
 /**
  * print_numbers - Prints from 0 to 9
- *
- * Return: numbers
  */
 void print_numbers(void)
 {
-	int a;
+	int c;
 
-	for (a = 0; a <= 9; a++)
-		_putchar(a);
+	for (c = 0; c <= 9; c++)
+		_putchar(c + '0');
 	_putchar('\n');
 }
