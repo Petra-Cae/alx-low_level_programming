9184d008750052bee5e944ae832fe137f1256629
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2022-10-24 10:03:32 -0700

    Reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,12 +2,11 @@
 
 /**
 *reset_to_98 - Takes a pointer to an int as parameter and updates its value
-*@n - interger
+*@n: interger
 *
-*Return: 0
+*Return: void
 */
 void reset_to_98(int *n)
 {
 	*n = 98;
-	return (0);
 }
