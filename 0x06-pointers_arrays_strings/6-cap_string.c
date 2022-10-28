e4d2a7d7c6ae8bbfc4174ffab913f1d9c33a05ce
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2022-10-28 01:29:16 +0000

    cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+* cap_string - capitalizes all words of a string
+* @string: the string to be modified
+*
+*Return: the converted string
+*/
+char *cap_string(char *string)
+{
+	int a;
+	int b;
+	char *c;
+
+	c[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+	while (*(string + a))
+	{
+		if (string[a] >= 'a' && string[a] <= 'z')
+		{
+			if (a == 0)
+				(string[a] -= 'a' - 'A');
+			else
+			{
+				for (b = 0; b <= 12; b++)
+				{
+					if (c[b] == *(string + a - 1))
+						(string[a] -= 'a' - 'A');
+				}
+			}
+		}
+		a++;
+	}
+	return (string);
+}
