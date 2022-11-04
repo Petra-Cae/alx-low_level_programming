6915488f578c9a03a2c8dbd2593e5b75a34c4dfc
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2022-11-04 16:50:11 +0000

    arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,13 +8,13 @@
 *
 *Return: 0
 */
-int main(int argc, char **argv)
+int main(int argc, char __attribute__((unused)) **argv)
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", argv[i - 1]);
+		printf("%d\n", i - 1);
 	}
 	return (0);
 }
