d701063bc104e6f3bc0ade6ab83764c3b320a11e
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2022-11-10 21:40:48 +0000

    reallocate memory block

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,6 +14,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		ptr = malloc(new_size);
+		return (ptr);
 	}
 	if (new_size == old_size)
 		return (ptr);
@@ -27,7 +28,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 		return (NULL);
 
-	ptr = malloc(new_size);
 	free(ptr);
+	ptr = malloc(new_size);
 	return (ptr);
 }
