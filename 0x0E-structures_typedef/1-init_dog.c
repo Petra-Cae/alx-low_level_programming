bbdb9fc86e724e9166a770fff05bc130e1a9f8e6
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2022-11-15 01:57:01 +0000

    dog init

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+* init_dog - initialises a var of type struct dog
+* @d: points to dog
+* @name: dog's name
+* @age: dog's age
+* @owner: dog's owner
+*
+* Return: void
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
+}
