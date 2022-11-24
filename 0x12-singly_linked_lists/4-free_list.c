adb4f18ab78db1ebe624f85562a1f9baf13d497f
Petra Obehioye Ojamanhor <petraoje4real@gmail.com>
2022-11-24 15:59:30 +0000

    free list_t

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* free_list - frees list_t
+* @head: points to the head of list_t
+*
+* Return: void
+*/
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(head->str);
+		free(head);
+	}
+}
